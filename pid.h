c63f44e13e7b4215f129e5221000631de1a48a4d
physalily <physalily@gmail.com>
2018-02-23 20:12:08 +0900

    [rev] fanctions wrote outside

--- pid.h
+++ pid.h
@@ -8,20 +8,26 @@ private:
   double d_diff;
   double prev_val;
 public:
-  PID(double kp, double ki, double kd){
-    this -> kp = kp;
-    this -> ki = ki;
-    this -> kd = kd;
-    d_diff = 0;
-    prev_val = 0;
-  }
-  void update(double cte){
-    p_diff = cte;
-    i_diff += cte;
-    d_diff = cte - prev_val;
-    prev_cte = cte;
-  }
-  double re(){
-    return kp *  p_diff + ki * i_diff + kd * d_diff;
-  }
+  PID(double kp, double ki, double kd);
+  void update(double cte);
+  double re();
 };
+
+inline PID::PID(double kp, double ki, double kd){
+  this -> kp = kp;
+  this -> ki = ki;
+  this -> kd = kd;
+  d_diff = 0;
+  prev_val = 0;
+}
+
+inline void PID::update(double cte){
+  p_diff = cte;
+  i_diff += cte;
+  d_diff = cte - prev_val;
+  prev_cte = cte;
+}
+
+inline double PID::re(){
+  return kp *  p_diff + ki * i_diff + kd * d_diff;
+}
